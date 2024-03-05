2a55384dce4022026db61bd646e60064e62246b5
swtemperaterainforest <160765769+swtemperaterainforest@users.noreply.github.com>
2024-03-05 16:22:15 +0000

    Delete resources directory

--- resources/ol-geocoder.js
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:cca620db75cbf86cb9d68b713c1a14823472f4feb84af3f17ec170fc56de1d15
-size 16282
--- resources/qgis2web.js
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:b744d2052bd51b8e2b2b43ff3a5c764690d4253870334dd032af62bedaa1c5c6
-size 53191
